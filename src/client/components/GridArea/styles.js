e8b568998bb74cab602ee94e5f4e9ca994ba8dd8
SECTI0NED <nikitaphung2000@gmail.com>
2021-10-29 16:28:09 +1100

    Updated styles

--- src/client/components/GridArea/styles.js
+++ src/client/components/GridArea/styles.js
@@ -2,7 +2,7 @@ const { makeStyles } = require("@material-ui/core");
 
 export const useStyles = makeStyles(() => ({
     square: {
-        border: "solid 1px red",
+        border: "solid 1px #4287f5",
         height: 15,
         width: 15,
     },
